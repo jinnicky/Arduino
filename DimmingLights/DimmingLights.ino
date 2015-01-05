97cea9ddd953d166ddcf55e6269149733097e391
Dmitry Kotenko <dmitry@kotenko.eu>
2015-01-05 19:45:06 +0300

    small code refactoring

--- DimmingLights/DimmingLights.ino
+++ DimmingLights/DimmingLights.ino
@@ -1,5 +1,10 @@
 #define LED_PIN 9
 
+#define ON 1
+#define OFF -1
+
+#define STEPS 25
+
 void setup()
 { 
   pinMode(LED_PIN, OUTPUT);
@@ -8,21 +13,22 @@ void setup()
 
 void loop()
 {
+  dimLights(STEPS, ON);
+  dimLights(STEPS, OFF);  
+}
+
+void dimLights(int steps, int onoff)
+{
+  int i = (onoff > 0)? 0 : steps;
+  int bpoint = (onoff > 0)? steps : 0;
+  int increment = (onoff > 0)? ON : OFF;
   
-  int i, steps = 255;
-  for (i = 0; i < steps; i++) 
-  {
+  while (i != bpoint) {
     analogWrite(LED_PIN, (255/steps) * i);
     delay(3000/steps);
-  }
-  
-  delay(100);
-  
-  for (i = steps; i > 0; i--) 
-  {
-    analogWrite(LED_PIN, (255/steps) * i);
-    delay(3000/steps);
+    i += increment;
   }
 
   delay(100);  
+  
 }
